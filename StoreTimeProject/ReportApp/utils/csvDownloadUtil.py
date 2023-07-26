d4c2bc5a3dfc71f223903be03461a3049d98d769
Naman Sharma <naman99sha@gmail.com>
2023-07-26 12:17:19 +0530

    Added Comments

--- StoreTimeProject/ReportApp/utils/csvDownloadUtil.py
+++ StoreTimeProject/ReportApp/utils/csvDownloadUtil.py
@@ -2,6 +2,8 @@ import gdown
 import os
 from datetime import datetime
 
+# Will download the updated csv from drive to folder in static directory by the name of datetime.utcnow()
+# Will return the download location to be used by csvToDbUtil.py
 def download_csv(url):
     
     # url = "https://drive.google.com/file/d/1UIx1hVJ7qt_6oQoGZgb8B3P2vd1FD025/view"

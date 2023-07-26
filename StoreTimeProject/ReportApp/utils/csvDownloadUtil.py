6caf269b6f687aa57bd8c2334d87e24e6fb28523
Naman Sharma <naman99sha@gmail.com>
2023-07-26 13:59:24 +0530

    Modifying csv download and store structure

--- StoreTimeProject/ReportApp/utils/csvDownloadUtil.py
+++ StoreTimeProject/ReportApp/utils/csvDownloadUtil.py
@@ -4,13 +4,17 @@ from datetime import datetime
 
 # Will download the updated csv from drive to folder in static directory by the name of datetime.utcnow()
 # Will return the download location to be used by csvToDbUtil.py
-def download_csv(url,filename):
+def download_csv(url):
     
     # url = "https://drive.google.com/file/d/1UIx1hVJ7qt_6oQoGZgb8B3P2vd1FD025/view"
-    file_id = url.split('/')[-2]
+    directoryName = str(datetime.utcnow())
+    download_location = "ReportApp/static/" + directoryName
+    os.makedirs(download_location)
+    retArr = []
+    for link in url:
+        file_id = link["url"].split('/')[-2]
 
-    prefix = 'https://drive.google.com/uc?/export=download&id='
-    download_location = "static/" + str(datetime.utcnow())
-    os.makedir(download_location)
-    gdown.download(prefix+file_id, output=download_location+"/"+filename+".csv")
-    return download_location
+        prefix = 'https://drive.google.com/uc?/export=download&id='
+        gdown.download(prefix+file_id, output=download_location+"/"+link["filename"]+".csv")
+        retArr.append(download_location+"/"+link["filename"]+".csv")
+    return retArr
--- StoreTimeProject/ReportApp/views.py
+++ StoreTimeProject/ReportApp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import *
+from .utils import csvDownloadUtil
 
 # Create your views here.
+def home(request):
+    urlArr = [
+        {"url": "https://drive.google.com/file/d/1UIx1hVJ7qt_6oQoGZgb8B3P2vd1FD025/view","filename": "store_status"},
+        {"url": "https://drive.google.com/file/d/101P9quxHoMZMZCVWQ5o-shonk2lgK1-o/view", "filename": "store_timezone"},
+        {"url": "https://drive.google.com/file/d/1va1X3ydSh-0Rt1hsy2QSnHRA4w57PcXg/view", "filename": "store_business_hours"}
+    ]
+    [storeStatusFilename, storeTimezoneFilename, storeBusinessHoursFilename] = csvDownloadUtil.download_csv(urlArr)
+    print("---------"+storeStatusFilename)
+    return HttpResponse("Done")
